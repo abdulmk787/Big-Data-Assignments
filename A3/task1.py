7d04dd97e8f7723a3fb4a3dc6543d2c86af2528b
ishita1864 <62196026+ishita1864@users.noreply.github.com>
2021-10-31 15:07:46 +0530

    Update task1.py

--- A3/task1.py
+++ A3/task1.py
@@ -13,9 +13,11 @@ df=sqlContext.read.format("csv").option("header","true").load(filepath)
 
 df2 = df.withColumn("AverageTemperature",df["AverageTemperature"].cast('float'))
 
-df3=df2[df2.Country.isin(country_name)].groupby('City').mean('AverageTemperature').withColumnRenamed("avg(AverageTemperature)", "tavg")
+dfp=df2.filter(df2.Country == country_name)
 
-df4= df2.join(df3, ["City"])
+df3=dfp.groupby('City').avg('AverageTemperature').withColumnRenamed("avg(AverageTemperature)", "tavg")
+
+df4= dfp.join(df3, ["City"])
 
 df5 =df4.filter( df4.AverageTemperature > df4.tavg).groupby('City').count().sort('City', ascending=True)
 res= df5.select("City", "count").collect()
@@ -23,4 +25,3 @@ res= df5.select("City", "count").collect()
 
 for row in res:
     print(row['City'] + "\t" +str(row['count']))
-

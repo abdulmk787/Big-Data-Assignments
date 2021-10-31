2b55784f558d36902d36f8c17dce47ea329e6305
ishita1864 <62196026+ishita1864@users.noreply.github.com>
2021-10-31 11:06:40 +0530

    Add files via upload

---
+++ A3/a3t1.py
@@ -0,0 +1,27 @@
+from pyspark import SparkContext
+from pyspark.sql import SQLContext
+import sys
+
+country_name= sys.argv[1]
+filepath = sys.argv[2]
+sc = SparkContext.getOrCreate()
+
+
+sqlContext = SQLContext(sc)
+
+
+df=sqlContext.read.format("csv").option("header","true").load(filepath)
+
+df2 = df.withColumn("AverageTemperature",df["AverageTemperature"].cast('float'))
+
+df3=df2[df2.Country.isin(country_name)].groupby('City').mean('AverageTemperature').withColumnRenamed("avg(AverageTemperature)", "tavg")
+
+df4= df2.join(df3, ["City"])
+
+df5 =df4.filter( df4.AverageTemperature > df4.tavg).groupby('City').count().sort('City', ascending=True)
+res= df5.select("City", "count").collect()
+
+
+for row in res:
+    print(row['City'] + ", " +str(row['count']))
+

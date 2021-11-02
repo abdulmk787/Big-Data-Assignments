df9bb5778c067fc40458442f6302fe113b4683ef
ishita1864 <62196026+ishita1864@users.noreply.github.com>
2021-11-02 13:14:59 +0530

    Update task2.py

--- A3/T2/task2.py
+++ A3/T2/task2.py
@@ -3,8 +3,8 @@ from pyspark.sql import SQLContext
 from pyspark.sql.functions import max
 import sys
 
-city_path= "./city_sample_5percent.csv"
-global_path = "./global.csv"
+city_path= sys.argv[1]
+global_path = sys.argv[2]
 
 sc = SparkContext.getOrCreate()
 sqlContext = SQLContext(sc)
@@ -14,13 +14,14 @@ dfc=sqlContext.read.format("csv").option("header","true").load(city_path)
 
 dfg=sqlContext.read.format("csv").option("header","true").load(global_path)
 
-dfc2= dfc.select("dt", "Country", "City", "AverageTemperature")
+dfc2= dfc.select("dt", "Country", "AverageTemperature").withColumn("AverageTemperature",dfc["AverageTemperature"].cast('float'))
 
 
-dfc3= dfc2.groupBy("dt", "Country", "City").agg(max("AverageTemperature")).withColumnRenamed("max(AverageTemperature)", "mavgt")
-dfc4= dfc3.join(dfg, ["dt"]).filter( dfc3.mavgt > dfg.LandAverageTemperature )
+dfc3= dfc2.groupBy( "dt", "Country").agg(max("AverageTemperature")).withColumnRenamed("max(AverageTemperature)", "mavgt")
+dfc31= dfc3.join(dfg, ["dt"]).withColumn("LandAverageTemperature",dfg["LandAverageTemperature"].cast('float'))
+dfc4= dfc31.filter( dfc3.mavgt > dfg.LandAverageTemperature )
 
-dfc5= dfc3.join(dfg, ["dt"]).filter( dfc3.mavgt > dfg.LandAverageTemperature ).groupby("Country").count().sort('Country', ascending=True)
+dfc5= dfc4.groupby("Country").count().sort('Country', ascending=True)
 
 res= dfc5.collect()
 
